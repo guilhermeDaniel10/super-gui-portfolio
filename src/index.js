3802327aab42becd0c280d6a6908e9b0174e7c4f
Guilherme Daniel <guilherme.daniel@inova.business>
2023-04-20 23:53:28 +0100

    Block movement

--- src/index.js
+++ src/index.js
@@ -1,6 +1,57 @@
+import { Player } from "./player.js";
+
 const canvas = document.querySelector("canvas");
 const c = canvas.getContext("2d");
-console.log(c);
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+const gravity = 1;
+
+const player = new Player();
+player.setCanvasContextValue(c);
+animate();
+addEventListeners();
 
 
-class Player
+function animate() {
+  requestAnimationFrame(animate);
+  c.clearRect(0, 0, canvas.width, canvas.height);
+  player.update(gravity);
+  player.playerSideMovementVelocity();
+}
+
+
+
+function addEventListeners() {
+  addEventListener("keydown", ({ key }) => {
+    switch (key) {
+      case "a":
+        player.moveLeft();
+        break;
+      case "d":
+        player.moveRight();
+        break;
+      case "w":
+        console.log("jump")
+        player.jump();
+        break;
+      case "s":
+      //player.duck();
+    }
+  });
+
+  addEventListener("keyup", ({ key }) => {
+    switch (key) {
+      case "a":
+        player.stopLeft();
+        break;
+      case "d":
+        player.stopRight();
+        break;
+      case "w":
+        //player.jump();
+        break;
+      case "s":
+      //player.duck();
+    }
+  });
+}
